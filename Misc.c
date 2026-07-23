2a8e2605545a2ea4983a34b7adafc07c95fa3019
João Tiago Pincho <joao.tiago.pincho@gmail.com>
2026-07-23 10:17:28 +0100

    added functions for rounding up quickly

--- Misc.c
+++ Misc.c
@@ -1,5 +1,6 @@
 #include "Misc.h"
 #include <stdlib.h>
+#include <Platform/Platform.h>
 
 int rand_range ( const int min, const int max )
 	{
@@ -42,4 +43,48 @@ double dclamp ( const double value, const double min, const double max )
 		return max;
 	else
 		return value;
+	}
+
+uint64_t round_pot_64 ( uint64_t input )
+	{
+	if ( input <= 1 )
+		return input;
+
+#if defined (PLATFORM_COMPILER_GNU)
+	uint64_t output = 1 << ( 64 - __builtin_clzl ( input - 1 ) );
+	return output;
+#else
+	uint64_t output = 1;
+	uint64_t shifted = input;
+	while ( shifted != 1 )
+		{
+		shifted >>= 1;
+		output <<= 1;
+		}
+	if ( output < input )
+		output <<= 1;
+	return output;
+#endif
+	}
+
+uint32_t round_pot_32 ( const uint32_t input )
+	{
+	if ( input <= 1 )
+		return input;
+
+#if defined (PLATFORM_COMPILER_GNU)
+	uint32_t output = 1 << ( 32 - __builtin_clz ( input - 1 ) );
+	return output;
+#else
+	uint32_t output = 1;
+	uint32_t shifted = input;
+	while ( shifted != 1 )
+		{
+		shifted >>= 1;
+		output <<= 1;
+		}
+	if ( output < input )
+		output <<= 1;
+	return output;
+#endif
 	}
